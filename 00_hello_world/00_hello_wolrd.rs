45f734e0d610ff03bdc6c9a5f778a07ab29224f7
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2024-11-21 09:54:32 -0300

    hello world

---
+++ 00_hello_world/00_hello_wolrd.rs
@@ -0,0 +1,10 @@
+//Olá mundo básico em Rust.
+//Os comentário serão em português mas o código, em sua maioria, inglês. 
+
+/*
+ A função "main" é obrigatório em um "crate binário", pois um executável será gerado.
+ A função "println!" na verdade é um macro.
+ */
+fn main() {
+    println!("Hello World! Olá Mundo!");
+}
